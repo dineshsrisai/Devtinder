1aea2413909c75ef13fd647452c5688ddc91a4af
dineshsrisai <dineshsrisai1@gmail.com>
2025-12-23 22:30:24 +0530

    Created Feed & user  API's

--- src/app.js
+++ src/app.js
@@ -5,6 +5,29 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({emailId : userEmail});
+        if(users.length === 0){
+            res.status(404).send("User not found!!!");
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(400).send("Something went wrong!!!");
+    }
+});
+
+app.get("/feed",async(req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(400).send("Something went wrong!!!");
+    }
+});
+
 app.post("/signup",async(req,res)=>{
     const user = new User(req.body);
     try{
