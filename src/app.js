30791b5667c4dd06363bf48700e406a8f9474058
dineshsrisai <dineshsrisai1@gmail.com>
2025-12-23 21:50:52 +0530

    Dynamic API

--- src/app.js
+++ src/app.js
@@ -3,14 +3,10 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
 
 app.post("/signup",async(req,res)=>{
-    const user = new User({
-        firstName:"DineshSriSai",
-        lastName :"Rudrakshala",
-        emailId : "dineshsrisai1@gmail.com",
-        password : "4122",
-    });
+    const user = new User(req.body);
     try{
         await user.save();
         res.send("User Added Successfully!!!");
