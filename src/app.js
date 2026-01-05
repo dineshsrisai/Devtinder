ec62b1ae7c68c1925b7bffd258820b756fa94b41
dineshsrisai <dineshsrisai1@gmail.com>
2026-01-05 22:16:22 +0530

    credentials : true

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({
+    origin : "http://localhost:5173/",
+    credentials : true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -17,7 +20,6 @@ app.use("/",authRouter);
 app.use("/",profileRouter);
 app.use("/",requestRouter);
 app.use("/",userRouter);
-
 
 connectDB()
     .then(()=>{
