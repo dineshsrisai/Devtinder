434c7c2dc39d041afee2d0162ade37aa52df68f5
dineshsrisai <dineshsrisai1@gmail.com>
2026-01-07 20:11:12 +0530

    res.status

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,55 +1,55 @@
 const express = require("express");
-const {validateSignUpData} = require("../utils/validation");
+const { validateSignUpData } = require("../utils/validation");
 const authRouter = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
-authRouter.post("/signup",async(req,res)=>{
-    try{
-        validateSignUpData(req);
-        const { firstName,lastName,emailId,password} = req.body;
-        const passwordHash = await bcrypt.hash(password,10);
-        const user = new User({
-            firstName,
-            lastName,
-            emailId,
-            password : passwordHash
-        });
-        await user.save();
-        res.send("User Added Successfully!!!");
-    } catch(err){
-        res.status(400).send("Error saving the user : " + err.message);
-    }
+authRouter.post("/signup", async (req, res) => {
+  try {
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+    await user.save();
+    res.send("User Added Successfully!!!");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message);
+  }
 });
 
-authRouter.post("/login",async(req,res)=>{
-    try{
-        const {emailId,password} = req.body;
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
 
-        const user = await User.findOne({emailId:emailId});
-        if(!user){
-            throw new Error("Invalid Credentials");
-        }
-        const isPasswordValid = await user.validatePassword(password);
-
-        if(isPasswordValid){
-            const token = await user.getJWT();
-            res.cookie("token",token,{
-                expires : new Date(Date.now() + 8 * 3600000)
-            });
-            res.send(user);
-        } else{
-            res.send("Invalid Credentials");
-        }
-    }catch(err){
-        res.status(400).send("Error"+err.message);
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credentials");
     }
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (isPasswordValid) {
+      const token = await user.getJWT();
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
+      res.send(user);
+    } else {
+      res.status(400).send("Invalid Credentials");
+    }
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
 });
 
-authRouter.post("/logout",async(req,res)=>{
-    res
-    .cookie("token",null,{
-        expires : new Date(Date.now()),
+authRouter.post("/logout", async (req, res) => {
+  res
+    .cookie("token", null, {
+      expires: new Date(Date.now()),
     })
     .send("Logout Successful!!!");
 });
