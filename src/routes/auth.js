ba58e034882c6eead5a2ca7c429b5de9a0e711f9
dineshsrisai <dineshsrisai1@gmail.com>
2026-03-24 23:23:31 +0530

    .

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -15,12 +15,15 @@ authRouter.post("/signup", async (req, res) => {
       emailId,
       password: passwordHash,
     });
+
     const savedUser = await user.save();
     const token = await savedUser.getJWT();
     res.cookie("token", token, {
       expires: new Date(Date.now() + 8 * 3600000),
     });
+
     res.json({ message: "User Added successfully", data: savedUser });
+
   } catch (err) {
     res.status(400).send("Error saving the user: " + err.message);
   }
